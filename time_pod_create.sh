6dc6149c229c57196cb3032196a0f041df34eb7a
Anlan Du <adu47249@gmail.com>
2022-11-01 15:03:19 -0700

    Roll ratify before every timing test to simulate longer initial call

--- time_pod_create.sh
+++ time_pod_create.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 for i in {1..100}; do
-  export KUBECTLPERFINDEX="75images$i"
-  yq -i '.metadata.name = strenv(KUBECTLPERFINDEX)' ./pods/75images.yaml
+  helm uninstall ratify -n ratify-service
+  helm install ratify ../ratify/charts/ratify --atomic --namespace ratify-service --create-namespace --set image.repository=anlandu/ratify --set image.tag=nosleep
+  export KUBECTLPERFINDEX="1image$i"
+  yq -i '.metadata.name = strenv(KUBECTLPERFINDEX)' ./pods/1image.yaml
   start=$(($(date +%s%N)/1000000))
-  kubectl apply -f ./pods/75images.yaml
+  kubectl apply -f ./pods/1image.yaml
   echo $(($(date +%s%N)/1000000 - $start))
-  kubectl delete -f ./pods/75images.yaml
+  kubectl delete -f ./pods/1image.yaml
 done
